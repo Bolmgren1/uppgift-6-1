d345ed495724718008c50b6213f7ca9de6e89865
Bergqvist1 <edvin.sven123@gmail.com>
2023-02-10 10:21:23 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            addera(4, 6, 15);
+        }
+        static void addera(int tal1, int tal2, int tal3)
+        {
+            Console.WriteLine($"{tal1}+{tal2}+{tal3} = {tal1 + tal2 + tal3}");
+        }
+        
+
+    }
+}
